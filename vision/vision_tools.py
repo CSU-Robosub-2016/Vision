41206a56f5601693367f202e496bbb61606df72a
bwgonzal <bwgonzal@rams.colostate.edu>
2017-02-16 20:30:09 -0700

    Update vision_tools.py

--- vision/vision_tools.py
+++ vision/vision_tools.py
@@ -14,7 +14,31 @@ class VisionTools:
     def __init__(self):
         # Dummy variable, not used for anything
         self.useme = True
+    ##
+    # @brief Detects largest contour and draws a box around it
+    # @param frame The frame in which a contour will be found
+    # @return output Returns a new image with a box around largest contour 
+    def lineIdent(self, frame):
+        frame = cv2.medianBlur(frame, 5)
 
+        # Threshold filter to find contours
+        ret, thresh = cv2.threshold(frame, 153, 255, 0)
+        bw = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+        
+        # Find Contours
+        q, contours, hierarchy = cv2.findContours(bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Find the index of the largest contour
+        areas = [cv2.contourArea(c) for c in contours]
+        max_index = np.argmax(areas)
+        cnt = contours[max_index]
+
+        # Create Rectangle around largest contour (ROI)
+        x, y, w, h = cv2.boundingRect(cnt)
+        output = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        return output
+        
     ##
     # @brief Orange line detection, developed by Brett Gonzales
     # @param frame The frame to be filtered
